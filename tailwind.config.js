7559149bef2773f18d6258b35749ba8a78044542
Pinqua <piyushsati1999@gmail.com>
2021-07-04 00:24:16 +0530

    removed unwanted code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
@@ -21,19 +21,10 @@ module.exports = {
       xxs: "375px",
       xs: "425px",
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "780px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
   },
   variants: {
