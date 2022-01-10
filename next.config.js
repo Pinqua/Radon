87bfaf70770b3c474a316a611bf2c32742e88e8b
Piyush Sati <69719134+Pinqua@users.noreply.github.com>
2022-01-10 23:22:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
+    runtimeCaching,
   },
   images: {
     domains: ["fakestoreapi.com"],
